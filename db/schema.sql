e3b693fdd3d7ef09baeb462de8c0744971f6dea6
Samuel Urom <urom.samuel@gmail.com>
2023-10-16 16:21:52 +1100

    created schema and cleaned up dataset csv

---
+++ db/schema.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE refill_router;
+
+\c refill_router;
+
+CREATE TABLE petrol_stations (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  owner TEXT NOT NULL,
+  address TEXT NOT NULL,
+  suburb TEXT NOT NULL,
+  state TEXT NOT NULL,
+  lat NUMBER NOT NULL,
+  lng NUMBER NOT NULL,
+);
