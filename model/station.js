7d355a87a7933d2a38d662ed186481debcf7eec6
Maureen Hegarty <maureen@communus.com.au>
2023-10-17 17:06:08 +1100

    remove commented outs in station.js

--- model/station.js
+++ model/station.js
@@ -13,17 +13,3 @@ function findAll() {
 module.exports = {
   findAll,
 };
-
-// function findAllByTen() {
-//   const sql = `
-//       SELECT *
-//       FROM petrol_stations
-//       ORDER BY id
-//       LIMIT 10;
-//       `;
-//   return db.query(sql).then((result) => result.rows);
-// }
-
-// module.exports = {
-//   findAll,
-// };
