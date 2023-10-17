8c207bca48773f60a9cb1cbbe2263190a8fd6f75
Justin Ong <justin-ong@live.com.au>
2023-10-17 12:46:06 +1100

    Added the time and date to the sidebar

--- client/client.js
+++ client/client.js
@@ -33,7 +33,6 @@ async function initMap() {
     longitudeCordinates.innerHTML = longitude;
   });
   ////////////////////////////
-
   fetch("http://localhost:8080/api/stations/all")
     .then((res) => res.json())
     .then((stations) => {
@@ -53,3 +52,54 @@ async function initMap() {
 }
 
 initMap();
+
+// Clock
+
+function startTime() {
+  let today = new Date();
+  let hr = today.getHours();
+  let min = today.getMinutes();
+  let sec = today.getSeconds();
+  ap = hr < 12 ? "<span>AM</span>" : "<span>PM</span>";
+  hr = hr > 12 ? hr - 12 : hr;
+  hr = checkTime(hr);
+  min = checkTime(min);
+  sec = checkTime(sec);
+
+  document.getElementById("clock").innerHTML =
+    hr + ":" + min + ":" + sec + " " + ap;
+
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let curWeekDay = days[today.getDay()];
+  let curDay = today.getDate();
+  let curMonth = months[today.getMonth()];
+  let curYear = today.getFullYear();
+  let date = curWeekDay + ", " + curDay + " " + curMonth + " " + curYear;
+  document.getElementById("date").innerHTML = date;
+
+  let time = setTimeout(function () {
+    startTime();
+  }, 500);
+}
+function checkTime(i) {
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
+}
+
+startTime();
