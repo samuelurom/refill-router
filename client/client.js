85977bff21a44cdb00963fd1b4dc3c3b9450eb71
Justin Ong <justin-ong@live.com.au>
2023-10-17 11:42:57 +1100

    Added markers on map

--- client/client.js
+++ client/client.js
@@ -7,6 +7,33 @@ async function initMap() {
     center: { lat: -34.397, lng: 150.644 },
     zoom: 8,
   });
+
+  const stationIcons = {
+    Caltex: "Caltex.png",
+    "7-Eleven Pty Ltd": "7-Eleven_Pty_Ltd.png",
+    United: "United.png",
+    Shell: "Shell.png",
+    Mobil: "Mobil.png",
+    Liberty: "Liberty.png",
+    BP: "BP.png",
+  };
+
+  fetch("http://localhost:8080/api/stations/all")
+    .then((res) => res.json())
+    .then((stations) => {
+      console.log(stations);
+      for (const station of stations) {
+        const marker = new google.maps.Marker({
+          position: { lat: station.lat, lng: station.lng },
+          map: map,
+          title: station.name,
+          icon: {
+            url: `/images/${stationIcons[station.owner] || "fuel_icon.png"}`,
+            scaledSize: new google.maps.Size(45, 45),
+          },
+        });
+      }
+    });
 }
 
 initMap();
