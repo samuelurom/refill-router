4d2be2c0b9bcf1aaffdd606084730b9be33091f7
Samuel Urom <urom.samuel@gmail.com>
2023-10-18 16:32:23 +1100

    created api route for nearest stations

--- model/station.js
+++ model/station.js
@@ -22,6 +22,22 @@ function findByBounds(swLat, neLat, swLng, neLng) {
     .then((result) => result.rows);
 }
 
+function findNearest(userLat, userLng) {
+  const sql = `SELECT *,
+      6371 * 2 * ASIN(SQRT(
+        POWER(SIN(RADIANS($1 - lat) / 2), 2) +
+        COS(RADIANS(lat)) * COS(RADIANS($1)) *
+        POWER(SIN(RADIANS($2 - lng) / 2), 2)
+      )) AS distance
+    FROM petrol_stations
+    ORDER BY distance
+    LIMIT 10;`;
+
+  const values = [userLat, userLng];
+
+  return db.query(sql, values).then((result) => result.rows);
+}
+
 function findRandom() {
   const sql = `
     SELECT * FROM petrol_stations
@@ -36,4 +52,5 @@ module.exports = {
   findAll,
   findByBounds,
   findRandom,
+  findNearest,
 };
--- routes/stations_router.js
+++ routes/stations_router.js
@@ -18,6 +18,12 @@ router.get(`/bounds`, (req, res) => {
   });
 });
 
+router.get("/nearest", (req, res) => {
+  const { userLat, userLng } = req.query;
+
+  Station.findNearest(userLat, userLng).then((stations) => res.json(stations));
+});
+
 router.get("/random", (req, res) => {
   Station.findRandom().then((station) => res.json(station));
 });
