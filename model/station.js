dbaa199b50df3e5c4e7aeed88d76aae2ebf11b46
Harley Brackley <harleybrackley@gmail.com>
2023-10-18 12:24:24 +1100

    Created route for /api/stations/bounds

--- model/station.js
+++ model/station.js
@@ -10,6 +10,19 @@ function findAll() {
   return db.query(sql).then((result) => result.rows);
 }
 
+function findByBounds(swLat, neLat, swLng, neLng) {
+  const sql = `
+        SELECT * FROM petrol_stations 
+        WHERE lat BETWEEN $1 AND $2 
+        AND lng BETWEEN $3 AND $4
+    `;
+
+  return db
+    .query(sql, [swLat, neLat, swLng, neLng])
+    .then((result) => result.rows);
+}
+
 module.exports = {
   findAll,
+  findByBounds,
 };
